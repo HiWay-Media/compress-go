91bbf6135dffa39d1b9f30954d1d2b41857c3d1c
Allan Nava <allannava95@gmail.com>
2023-10-16 17:17:41 +0200

    added api stuff

---
+++ compress/api_category.go
@@ -0,0 +1,17 @@
+package compress
+
+/**
+*
+* @param {string} apikey
+* @param {string} customer
+* @returns list of categories of the customer
+*/
+
+
+
+/**
+*
+* @param {string} apikey
+* @param {string} customer
+* @returns list of categories of the customer
+*/
---
+++ compress/api_upload.go
@@ -0,0 +1,17 @@
+package compress
+
+
+/**
+	* upload video with compress encoding
+	*
+	* if destination folder is empty, it will upload to the root of the bucket
+	*
+	* remember to specify the folder (usually upload)
+	*
+	* @param {string} destination_folder 
+	* @param {file} file 
+	* @param {string} title 
+	* @param {string} tags 
+	* @param {string} location_place 
+	* @param {number} category_id 
+*/
