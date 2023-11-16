4a6c15ae27c8a8ba4040a86032166c0cf0c40e16
Allan Nava <allannava95@gmail.com>
2023-11-16 15:24:28 +0100

    solve

--- compress/compress_test.go
+++ compress/compress_test.go
@@ -3,16 +3,28 @@ package compress
 import (
 	"os"
 	"testing"
+	"fmt"
 )
 
 func TestNewCompress(t *testing.T) {
 	//
-	apiKey 			:= os.Getenv("API_KEY")
-	customerName	:= os.Getenv("CUSTOMER_NAME")
-	//
-	c, err := NewCompress(customerName, apiKey, false)
+	c, err := GetCompress()
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	c.IsDebug()
+}
+
+
+func GetCompress() ( ICompress, error ) {
+	//
+	apiKey 			:= os.Getenv("API_KEY")
+	customerName	:= os.Getenv("CUSTOMER_NAME")
+	//
+	c, err := &NewCompress(customerName, apiKey, false)
+	if err != nil {
+		return nil, err
+	}
+	//
+	return c, nil
 }
