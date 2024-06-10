3357f04f400655ba7edfff18d08228dd7eb7010b
Allan Nava <allannava95@gmail.com>
2024-06-10 14:57:08 +0200

    added customer get zone

--- test/api_customer_test.go
+++ test/api_customer_test.go
@@ -1,2 +1,17 @@
 package test
 
+
+func TestGetCustomerS3(t *testing.T) {
+	//
+	c, err := GetCompress()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	// need to be finish
+	zone, err := c.GetCustomerS3Zone()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	log.Println("zone ", zone)
+	//c.IsDebug()
+}
