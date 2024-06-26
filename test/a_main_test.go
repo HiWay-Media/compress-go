89b8687d94227ce20ffa12efb285ec0c7fccaca8
Allan Nava <allannava95@gmail.com>
2024-06-26 13:22:36 +0200

    removed useless log

--- test/a_main_test.go
+++ test/a_main_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"os"
 	"testing"
-	"log"
 	"github.com/HiWay-Media/compress-go/compress"
 )
 
@@ -21,7 +20,7 @@ func TestMain(m *testing.M) {
 func TestNewCompress(t *testing.T) {
 	c, err := GetCompress()
 	if err != nil {
-		log.Println("error getComrpess", err.Error())
+		//log.Println("error getComrpess", err.Error())
 		t.Fatalf(err.Error())
 	}
 	c.IsDebug()
